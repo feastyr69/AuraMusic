4760ed2df5152d88f59d0a38d9944f7536006d1b
feastyr69 <yashraj3493@gmail.com>
2026-05-20 00:48:55 +0530

    refactor:database switched to neon

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,11 @@
 const { Pool } = require('pg');
+const { neon } = require('@neondatabase/serverless');
 const dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.POSTGRES_URL,
+    ssl: true,
 });
 
 const initDb = async () => {
